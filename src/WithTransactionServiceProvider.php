99acfa71e2463571b96ae3620e4bd2697a82b658
Yassen Sayed <devysm2019@gmail.com>
2025-06-28 16:26:16 +0300

    Create the transaction package

---
+++ src/WithTransactionServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace YSM\WithTransaction;
+
+use Illuminate\Support\ServiceProvider;
+
+class WithTransactionServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        // Optional: publish config, routes, etc.
+    }
+
+    public function register(): void
+    {
+        //
+    }
+}
